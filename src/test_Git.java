63c3deaca6bc0eea00bb6e8fa9f5eec447f7002a
felbor <felbor-r@yandex.ru>
2022-03-16 22:40:10 +0300

    22

--- src/test_Git.java
+++ src/test_Git.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class test_Git {
     public static void main(String[] args) {
-        int a, b;
+        int a, b, c, d;
         Scanner scanner = new Scanner(System.in);
         System.out.print("a = ");
         a = scanner.nextInt();
         System.out.print("b = ");
         b = scanner.nextInt();
-        System.out.println(a + b);
+        System.out.print("c = ");
+        c = scanner.nextInt();
+        System.out.print("d = ");
+        d = scanner.nextInt();
+        System.out.println(a + b + c + d);
     }
 }
